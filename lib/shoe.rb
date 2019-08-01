da17339c07f338b243680ee26f4d8882b8c56c74
Olivia Kesler <oakesler@protonmail.ch>
2019-08-01 19:21:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,12 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   def initialize(brand)
     @brand = brand
   end
+  
+  def cobble
+    puts "The "
+  
 end
 
