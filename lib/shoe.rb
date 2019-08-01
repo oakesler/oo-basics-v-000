6e4ccf6a2d7b9ef3c19418ff99720c6a567d41a5
Olivia Kesler <oakesler@protonmail.ch>
2019-08-01 19:24:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,8 @@ class Shoe
   
   
   def cobble
-    puts "The shoe has been repaired."
-    Shoe.condition = "new"
+    puts "Your shoe is as good as new!"
+    #Shoe.condition = "new"
   end
 end
 
